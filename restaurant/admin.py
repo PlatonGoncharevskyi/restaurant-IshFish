efeb4e1320d23358659471bf5a235d7d0057ac82
Platon Goncharevskyi <platongoncharevskyi@gmail.com>
2025-07-28 18:32:12 +0300

    customize admin panel

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from restaurant.models import DishType, Cook, Dish
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "price", "dish_type", ]
+    list_filter = ["dish_type", ]
+    search_fields = ["name", ]
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience", )
+
+admin.site.register(DishType)
